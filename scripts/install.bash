7f843005b2f31dd77d8ebd952741d055e4d65831
Hiroyuki Sano <sh19910711@gmail.com>
2014-08-06 14:45:42 +0900

    add install.bash

---
+++ scripts/install.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function copy_dotfile() {
+  local src=~/dotfiles/src/$1
+  local dst=~/$1
+
+  # put out existing file
+  # e.g. ~/.vimrc -> ~/.vimrc.12345
+  readlink ${dst} >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    if [[ -e ${dst} ]]; then
+      echo "move: ${dst} -> ${dst}.`date +"%s"`"
+      mv ${dst} ${dst}.`date +"%s"`
+    fi
+  fi
+
+  # create link
+  [[ ! -e ${dst} ]] && ln -s ${src} ${dst} && echo "create: ${dst}"
+}
+
+DOTFILES=(".vimrc")
+
+for dotfile in ${DOTFILES}; do
+  copy_dotfile ${dotfile}
+done
+
